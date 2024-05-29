0135a893e57e9f24f4e8a9bde8a41fc07fc75a54
Ticiano Lage <ticianoarraeslage@gmail.com>
2024-05-29 10:46:57 -0300

    Bug fix - qtd nula

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ def get_yes_no_input(message):
     else:
       print("Entrada inválida. Por favor entrar S ou N.")
 
+def zeroIfNullInput(message):
+  user_input = input(message)
+  if not user_input:
+    return 0
+  else:
+    return int(user_input)
 
 char = input("Nome do Char: ")
 spellhotkey = input("Hotkey para magia: ")
@@ -25,10 +31,10 @@ ringhotkey = input("Hotkey para life ring: ")
 foodhotkey = input("Hotkey para comer: ")
 Promoted = get_yes_no_input("Char com promotion? (S/N) ")
 Bonus = get_yes_no_input("Char com bonus semanal de mana? (S/N) ")
-blankrunes = int(input("Quantidade de blank runes: "))
-liferings = int(input("Qtd de life rings: "))
-TempoLifeRings = int(input("Tempo atual do primeiro life ring (segundos). Deixar em branco se brand new: "))
-mana = int(input("Mana atual (aproximada): "))
+blankrunes = zeroIfNullInput("Quantidade de blank runes: ")
+liferings = zeroIfNullInput("Qtd de life rings: ")
+TempoLifeRings = zeroIfNullInput("Tempo atual do primeiro life ring (segundos). Deixar em branco se brand new: ")
+mana = zeroIfNullInput("Mana atual (aproximada): ")
 
 intfcr = Interfacer(foodHotkey=foodhotkey, spellHotkey=spellhotkey, ringHotkey=ringhotkey, charName=char)
 sch = Runer(
