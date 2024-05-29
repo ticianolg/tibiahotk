f3e64bb97888c177abd42205bb9bb383cdd4a2b6
Ticiano Lage <ticianoarraeslage@gmail.com>
2024-05-29 10:42:21 -0300

    Variaveis via input

--- interfacer.py
+++ interfacer.py
@@ -1,24 +1,29 @@
 import pyautogui
 import win32gui
 
+
 class Interfacer:
     def __init__(self, foodHotkey, spellHotkey, ringHotkey, charName):
         self.foodHotkey = foodHotkey
         self.spellHotkey = spellHotkey
         self.ringHotkey = ringHotkey
+        self.WindowName = f"Tibia - {charName}"
         self.hwnd= win32gui.FindWindow(None, f"Tibia - {charName}")
 
     async def Eat(self):
         win32gui.ShowWindow(self.hwnd, 5)
+        pyautogui.press("alt")
         win32gui.SetForegroundWindow(self.hwnd)  # Activate the window
         pyautogui.press(self.foodHotkey)
 
     def Rune(self):
         win32gui.ShowWindow(self.hwnd, 5)
+        pyautogui.press("alt")
         win32gui.SetForegroundWindow(self.hwnd)  # Activate the window
         pyautogui.press(self.spellHotkey)
 
     def Ring(self):
         win32gui.ShowWindow(self.hwnd, 5)
+        pyautogui.press("alt")
         win32gui.SetForegroundWindow(self.hwnd)  # Activate the window
         pyautogui.press(self.ringHotkey)
--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 from interfacer import Interfacer
 from scheduler import Runer
 
+def get_yes_no_input(message):
+  """Gets user input (S/N) case-insensitively.
+
+  Args:
+      message: The message to display to the user.
+
+  Returns:
+      True if the user enters 's' or 'S', False otherwise.
+  """
+
+  while True:
+    user_input = input(f"{message} (S/N): ").lower()  # Convert to lowercase
+    if user_input in ('s', 'n'):
+      return user_input == 's'  # Return True for 'y'
+    else:
+      print("Entrada inválida. Por favor entrar S ou N.")
+
+
 char = input("Nome do Char: ")
-intfcr = Interfacer(foodHotkey="o", spellHotkey="9", ringHotkey="v", charName=char)
+spellhotkey = input("Hotkey para magia: ")
+ringhotkey = input("Hotkey para life ring: ")
+foodhotkey = input("Hotkey para comer: ")
+Promoted = get_yes_no_input("Char com promotion? (S/N) ")
+Bonus = get_yes_no_input("Char com bonus semanal de mana? (S/N) ")
+blankrunes = int(input("Quantidade de blank runes: "))
+liferings = int(input("Qtd de life rings: "))
+TempoLifeRings = int(input("Tempo atual do primeiro life ring (segundos). Deixar em branco se brand new: "))
+mana = int(input("Mana atual (aproximada): "))
+
+intfcr = Interfacer(foodHotkey=foodhotkey, spellHotkey=spellhotkey, ringHotkey=ringhotkey, charName=char)
 sch = Runer(
-    promoted=True, 
-    lifeRingAmount=3, 
-    restingBonus=True, 
-    blankRunes=255, 
+    promoted=Promoted, 
+    lifeRingAmount=liferings, 
+    restingBonus=Bonus, 
+    blankRunes=blankrunes, 
     intfcr=intfcr)
-sch.setMana(468)
-sch.setLifeRing(4*60 + 6)
+sch.setMana(mana)
+if TempoLifeRings > 0:
+    sch.setLifeRing(4*60 + 6)
 sch.Start()
